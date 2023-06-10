37bcc4c9ff717540504d150a348dab0e523df45a
邴新科-工作 <kylebing@163.com>
2023-06-10 15:21:27 +0800

    update 说明

--- src/mapConfig.js
+++ src/mapConfig.js
@@ -2,4 +2,5 @@ export default {
     key: ''
     // 申请的 web js 服务的 key，目前好像只用 Key 就能用，如果不能用请参阅官方配置教程
     // https://lbs.amap.com/api/javascript-api/guide/abc/prepare
+    // 要想展示路线，需要在服务器上配置，具体看上面这个链接，不然无法展示路线。
 }
